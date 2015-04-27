cd9e41a78c977eb974d5cba1d86d95c853515f39
adamyarger <yargeradam@gmail.com>
2015-04-27 13:37:43 -0500

    now its going on github

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,6 +2,8 @@ class Flight < ActiveRecord::Base
 
 	belongs_to :to_airport,   class_name: "Airport"
 	belongs_to :from_airport, class_name: "Airport"
+	has_many :passengers, :through => :bookings
+	has_many :bookings
 
 	def self.get_dates
 		pluck(:start_time).map{ |d| [d.strftime("%m/%d/%Y"), d.to_date]}.uniq
---
+++ app/models/passenger.rb
@@ -0,0 +1,7 @@
+class Passenger < ActiveRecord::Base
+
+	has_many :tickets
+	has_many :flights,  :through => :bookings
+	has_many :bookings, :through => :tickets
+
+end
